57a8f27ff149e9650918ee42f17d818d6e482a2e
PTRDDK <piootr.dudek@gmail.com>
2018-12-16 21:17:30 +0100

    BattleshipProbabilityMap comments added

--- src/main/java/pl/beny/nsai/game/battleship/BattleshipProbabilityMap.java
+++ src/main/java/pl/beny/nsai/game/battleship/BattleshipProbabilityMap.java
@@ -5,21 +5,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//instance of probability table (1 per player and 1 move)
 public class BattleshipProbabilityMap {
 
-    private int skewFactor = 2;
-    private int[][] probabilities;
-    private int[][] board;
-    private List<Coordinate> hits = new ArrayList<>();
-    private List<Coordinate> adj = new ArrayList<>();
-    private BattleshipBoard battleshipBoard;
-    private List<Integer> availableShipSizes = new ArrayList<>();
+    private int skewFactor = 2;                                                 //factor of multiplying probability after hit around this coordinates
+    private int[][] probabilities;                                              //table of probabilities for every coordinate
+    private int[][] board;                                                      //current game board status
+    private List<Coordinate> hits = new ArrayList<>();                          //list of all hits
+    private List<Coordinate> adj = new ArrayList<>();                           //list of probable next places of ship
+    private BattleshipBoard battleshipBoard;                                    //current battleship board instance
+    private List<Integer> availableShipSizes = new ArrayList<>();               //list of not sunk ships
 
     public BattleshipProbabilityMap(BattleshipBoard battleshipBoard) {
         this.battleshipBoard = battleshipBoard;
         this.availableShipSizes = battleshipBoard.getAvailableShips();
     }
 
+    //initialize of generating new probablity map
     public void init() {
         probabilities = new int[BattleshipBoard.BOARD_SIZE][BattleshipBoard.BOARD_SIZE];
         for (int x = 0; x < BattleshipBoard.BOARD_SIZE; x++) {
@@ -30,19 +32,22 @@ public class BattleshipProbabilityMap {
         recalculateProbabilities();
     }
 
+    //array of probabilities
     public int[][] getProbabilities() {
         return probabilities;
     }
 
+    //list of all hits
     public List<Coordinate> getHits() {
         return hits;
     }
 
+    //recalculation of probability map based on board status
     private void recalculateProbabilities() {
         board = new int[BattleshipBoard.BOARD_SIZE][BattleshipBoard.BOARD_SIZE];
-        board = loadBoard();
+        board = loadBoard();                                                            //load of current game board status
 
-        // przepisanie aktualnej planszy
+        // rewrite all hits from board to hit list
         for (int y = 0; y < BattleshipBoard.BOARD_SIZE; y++) {
             for (int x = 0; x < BattleshipBoard.BOARD_SIZE; x++) {
                 if (board[y][x] == 2) {
@@ -51,28 +56,29 @@ public class BattleshipProbabilityMap {
             }
         }
 
-        //wyliczenie prawdopodobieństwa dla każdego typu statku
+        //calculation of probability for every coordinate
         int listSize = availableShipSizes.size();
         for (int i = 0; i < listSize; i++) {
             for (int x = 0; x < BattleshipBoard.BOARD_SIZE; x++) {
                 for (int y = 0; y < BattleshipBoard.BOARD_SIZE; y++) {
-                    // poziomo
-                    if (shipCanBePlaced(3, x, y, availableShipSizes.get(i), false)) {
-                        increaseProbablity(x, y, availableShipSizes.get(i), false);
+                    // horizontal
+                    if (shipCanBePlaced(3, x, y, availableShipSizes.get(i), false)) {       //if ship part can be in coords increase probability
+                        increaseProbablity(x, y, availableShipSizes.get(i), false);                         //probability increase
                     }
-                    //pionowo
-                    if (shipCanBePlaced(3, x, y, availableShipSizes.get(i), true)) {
-                        increaseProbablity(x, y, availableShipSizes.get(i), false);
+                    // vertical
+                    if (shipCanBePlaced(3, x, y, availableShipSizes.get(i), true)) {        //if ship part can be in coords increase probability
+                        increaseProbablity(x, y, availableShipSizes.get(i), false);                         //probability increase
                     }
                 }
             }
         }
 
-        skewProbabilityAroundHits(hits);
+        skewProbabilityAroundHits(hits);                        //increasing probability around previous hits
 
-        adaptToCurrentBoard();
+        adaptToCurrentBoard();                                  //change probability to 0 on coords where was already shot
     }
 
+    //change probability to 0 on coords where was already shot
     private void adaptToCurrentBoard() {
         for (int y = 0; y < BattleshipBoard.BOARD_SIZE; y++) {
             for (int x = 0; x < BattleshipBoard.BOARD_SIZE; x++) {
@@ -85,21 +91,22 @@ public class BattleshipProbabilityMap {
         }
     }
 
+    //increasing probability around previous hits
     private void skewProbabilityAroundHits(List<Coordinate> hits) {
         List<Coordinate> toSkew = new ArrayList<>();
         Set<Coordinate> skewNoDuplicate = new HashSet<>();
         toSkew = hits;
         List<Coordinate> join = new ArrayList<>(toSkew);
         for (int i = 0; i < hits.size(); i++) {
-            List<Coordinate> tmp = getAdjacentPosition(toSkew.get(i));
+            List<Coordinate> tmp = getAdjacentPosition(toSkew.get(i));          //finding coords up/down/left/right to previous hit
             join.addAll(tmp);
         }
 
         for (int k = 0; k < join.size(); k++) {
-            skewNoDuplicate.add(join.get(k));
+            skewNoDuplicate.add(join.get(k));                                   //removing possible duplicates of possible shots
         }
 
-        for (Coordinate coordinate : skewNoDuplicate) {
+        for (Coordinate coordinate : skewNoDuplicate) {                         //multiplying posibility around previous shot and reset prob for previous hit to 0
 
             if (!hits.contains(coordinate)) {
                 probabilities[coordinate.getY()][coordinate.getX()] *= skewFactor;
@@ -109,6 +116,7 @@ public class BattleshipProbabilityMap {
         }
     }
 
+    //finding best next shot around previous hit based on max ship size
     private List<Coordinate> getAdjacentPosition(Coordinate coordinate) {
 
         for (Coordinate coord : hits) {
@@ -124,6 +132,7 @@ public class BattleshipProbabilityMap {
         return adj;
     }
 
+    //finding best next Y coordinate around previous hit
     private void findPossibleShipPlacementsByY(Coordinate coordinate) {
         if (coordinate.getY() + 1 < BattleshipBoard.BOARD_SIZE) {
             adj.add(new Coordinate(coordinate.getX(), coordinate.getY() + 1));
@@ -133,6 +142,7 @@ public class BattleshipProbabilityMap {
         }
     }
 
+    //finding best next X coordinate around previous hit
     private void findPossibleShipPlacementsByX(Coordinate coordinate) {
         if (coordinate.getX() + 1 < BattleshipBoard.BOARD_SIZE) {
             adj.add(new Coordinate(coordinate.getX() + 1, coordinate.getY()));
@@ -142,6 +152,7 @@ public class BattleshipProbabilityMap {
         }
     }
 
+    //increasing probability
     private void increaseProbablity(int x, int y, int shipSize, boolean vertical) {
         int z = (vertical ? y : x);
         int end = z + shipSize - 1;
@@ -155,6 +166,7 @@ public class BattleshipProbabilityMap {
         }
     }
 
+    //checking if ship can be placed to increase probability
     private boolean shipCanBePlaced(int criteriaForRejection, int x, int y, int shipSize, boolean vertical) {
         int x1 = x + shipSize - 1;
         int y1 = y + shipSize - 1;
@@ -163,7 +175,6 @@ public class BattleshipProbabilityMap {
             return false;
         }
 
-        // poziomo
         if (!vertical) {
             return battleshipBoard.placeAvailableForMap(criteriaForRejection, x, y, shipSize, false);
         } else if (vertical) {
@@ -172,6 +183,7 @@ public class BattleshipProbabilityMap {
         return true;
     }
 
+    //load current game board before move
     private int[][] loadBoard() {
         for (int x = 0; x < BattleshipBoard.BOARD_SIZE; x++) {
             for (int y = 0; y < BattleshipBoard.BOARD_SIZE; y++) {
@@ -181,6 +193,7 @@ public class BattleshipProbabilityMap {
         return board;
     }
 
+    //class for better control on coordinates
     static class Coordinate {
         public int X;
         public int Y;
