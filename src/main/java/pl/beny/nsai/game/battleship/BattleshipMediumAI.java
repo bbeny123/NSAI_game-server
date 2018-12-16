3c8833e60570a54a3fc3498922fb3c2001c90296
nsai18-7 <nsai18-7@sr.dmcs.pl>
2018-12-16 21:38:32 +0100

    added comments for BattleshipMediumAI

--- src/main/java/pl/beny/nsai/game/battleship/BattleshipMediumAI.java
+++ src/main/java/pl/beny/nsai/game/battleship/BattleshipMediumAI.java
@@ -8,21 +8,22 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import static pl.beny.nsai.dto.battleship.BattleshipFireResponse.BattleshipFireTurn.PLAYER_TURN;
 
+//medium AI
 public class BattleshipMediumAI {
 
-    private int fireStatus;
+    private int fireStatus; //last fire status value after calling 'board.fire(x, y)' method
 
-    private int strategy = 0;
+    private int strategy = 0; //strategies to shoot: 0 - down, 1- up, 2 - right, 3 - left
 
-    private boolean underFire;
+    private boolean underFire;  //if true method fire uses strategies to sunk ship
 
-    private int accurateHitX;
+    private int accurateHitX; //primary successful hit - coordinate x
 
-    private int accurateHitY;
+    private int accurateHitY; //primary successful hit - coordinate y
 
-    private int lastSuccessfulStrategyHitX;
+    private int lastSuccessfulStrategyHitX; //last successful hit from current strategy - coordinate x
 
-    private int lastSuccessfulStrategyHitY;
+    private int lastSuccessfulStrategyHitY; //last successful hit from current strategy - coordinate y
 
     public BattleshipFireResponse fire(BattleshipBoard board) {
         for (int i = 0; i < 1000; i++) {
@@ -30,12 +31,12 @@ public class BattleshipMediumAI {
                 int x = ThreadLocalRandom.current().nextInt(BattleshipBoard.BOARD_SIZE);
                 int y = ThreadLocalRandom.current().nextInt(BattleshipBoard.BOARD_SIZE);
                 if (fireStatus > 0) {
-                    board.markAsMissAroundSunkShip(lastSuccessfulStrategyHitX, lastSuccessfulStrategyHitY);
+                    board.markAsMissAroundSunkShip(lastSuccessfulStrategyHitX, lastSuccessfulStrategyHitY); //after successful sunk mark fields around ship as miss to prevent shooting at this area
                     resetStrategy();
                 }
                 if (!underFire) {
                     fireStatus = board.fire(x, y);
-                    if (BattleshipBoard.FireStatus.HIT == fireStatus) {
+                    if (BattleshipBoard.FireStatus.HIT == fireStatus) { //if hit was successful assign variables and switch method to strategy mode
                         lastSuccessfulStrategyHitX = x;
                         accurateHitX = x;
                         lastSuccessfulStrategyHitY = y;
@@ -43,9 +44,9 @@ public class BattleshipMediumAI {
                         underFire = true;
                     }
                     return new BattleshipFireResponse(x, y, fireStatus, PLAYER_TURN);
-                } else {
+                } else { //strategy mode
                     if (strategy == 0) {
-                        if (accurateHitX < BattleshipBoard.BOARD_SIZE - 1 && lastSuccessfulStrategyHitX < BattleshipBoard.BOARD_SIZE - 1) {
+                        if (accurateHitX < BattleshipBoard.BOARD_SIZE - 1 && lastSuccessfulStrategyHitX < BattleshipBoard.BOARD_SIZE - 1) { //verify boundaries
                             x = lastSuccessfulStrategyHitX + 1;
                             y = lastSuccessfulStrategyHitY;
                             fireStatus = board.fire(x, y);
@@ -62,7 +63,7 @@ public class BattleshipMediumAI {
                     }
 
                     if (strategy == 1) {
-                        if (accurateHitX >= 1 && lastSuccessfulStrategyHitX >= 1) {
+                        if (accurateHitX >= 1 && lastSuccessfulStrategyHitX >= 1) { //verify boundaries
                             x = lastSuccessfulStrategyHitX - 1;
                             y = lastSuccessfulStrategyHitY;
                             fireStatus = board.fire(x, y);
@@ -79,7 +80,7 @@ public class BattleshipMediumAI {
                     }
 
                     if (strategy == 2) {
-                        if (accurateHitY < BattleshipBoard.BOARD_SIZE - 1 && lastSuccessfulStrategyHitY < BattleshipBoard.BOARD_SIZE - 1) {
+                        if (accurateHitY < BattleshipBoard.BOARD_SIZE - 1 && lastSuccessfulStrategyHitY < BattleshipBoard.BOARD_SIZE - 1) { //verify boundaries
                             x = lastSuccessfulStrategyHitX;
                             y = lastSuccessfulStrategyHitY + 1;
                             fireStatus = board.fire(x, y);
@@ -96,7 +97,7 @@ public class BattleshipMediumAI {
                     }
 
                     if (strategy == 3) {
-                        if (accurateHitY >= 1 && lastSuccessfulStrategyHitY >= 1) {
+                        if (accurateHitY >= 1 && lastSuccessfulStrategyHitY >= 1) { //verify boundaries
                             x = lastSuccessfulStrategyHitX;
                             y = lastSuccessfulStrategyHitY - 1;
                             fireStatus = board.fire(x, y);
@@ -127,11 +128,13 @@ public class BattleshipMediumAI {
         throw new GamesException(GamesException.GamesErrors.AI_ERROR);
     }
 
+    //restores default values
     private void resetStrategy() {
         underFire = false;
         strategy = 0;
     }
 
+    //moves to the next strategy and reassign value of primary successful shoot
     private void changeStrategy() {
         strategy++;
         lastSuccessfulStrategyHitX = accurateHitX;
