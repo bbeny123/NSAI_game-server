bb1b4fdea4511a3a3cbe6a496f5b5fc60dd8593a
Jacek Kulis <jacek.jk.kulis@gmail.com>
2018-12-17 17:02:56 +0100

    cleanup and add comments

--- src/main/java/pl/beny/nsai/game/checkers/FuzzyLogic.java
+++ src/main/java/pl/beny/nsai/game/checkers/FuzzyLogic.java
@@ -2,11 +2,23 @@ package pl.beny.nsai.game.checkers;
 
 import net.sourceforge.jFuzzyLogic.FIS;
 
+/**
+ * This class provides wrapper for FuzzyLogic library.
+ */
 public class FuzzyLogic {
+
+    /**
+     * @param playerPieces     Number of player pawns.
+     * @param cpuPieces        Number of computer pawns.
+     * @param currentDoneTurns Number of done turns.
+     * @return Value that determines stage of game.
+     */
     static double getStageVal(double playerPieces, double cpuPieces, double currentDoneTurns) throws Exception {
+        //Open file with rules
         String fileName = "fcl/stage.fcl";
         FIS fis = FIS.load(fileName, true);
 
+        //Check if file is loaded
         if (fis == null) {
             throw new Exception("Can't load file: '\" + fileName + \"'\"");
         }
@@ -16,7 +28,7 @@ public class FuzzyLogic {
         fis.setVariable("cpuPieces", cpuPieces);
         fis.setVariable("turns", currentDoneTurns);
 
-        // Evaluate
+        // Evaluate value
         fis.evaluate();
 
         return fis.getVariable("stage").getLatestDefuzzifiedValue();
--- src/main/java/pl/beny/nsai/game/checkers/MinMaxAlgorithm.java
+++ src/main/java/pl/beny/nsai/game/checkers/MinMaxAlgorithm.java
@@ -11,38 +11,67 @@ import java.util.concurrent.ThreadLocalRandom;
 import static pl.beny.nsai.game.checkers.CheckersMan.Side;
 import static pl.beny.nsai.game.checkers.CheckersMan.Side.BLACK;
 
+
+/**
+ * This class provides checker's computer movement powered by MIN-MAX algorithm (with Fuzzy logic optionally)
+ */
 public class MinMaxAlgorithm {
+
+    /**
+     * Depth of min-max search tree. The bigger number the longer it takes to have an output
+     */
     private int depth;
+
+    /**
+     * Use fuzzy logic if true
+     */
     private boolean fuzzy;
+
+    /**
+     * Side of computer
+     */
     private Side side;
 
+
+    /**
+     * @param s Side of AI
+     */
     public MinMaxAlgorithm(Side s) {
-        this.depth = 2;
+        this.depth = 3;
         this.fuzzy = false;
         this.side = s;
     }
 
+    /**
+     * @param s     Side of AI
+     * @param fuzzy True if using fuzzy logic
+     */
     public MinMaxAlgorithm(Side s, boolean fuzzy) {
-        this.depth = 2;
+        this.depth = 3;
         this.fuzzy = fuzzy;
         this.side = s;
     }
 
-    public MinMaxAlgorithm(Side s, int depth) {
-        this.side = s;
-        this.depth = depth;
-    }
-
+    /**
+     * Moves draughtsman on board.
+     *
+     * @param board         Reference to checkers board
+     * @param forcedCapture If set move with forced capture
+     * @return Returns CheckersResult with information where to move draughtsman
+     */
     public CheckersResult makeMove(CheckersBoard board, CheckersMoves forcedCapture) {
+        // Check if there are forced captures
         if (forcedCapture != null) {
-            CheckersMoves move = new CheckersMoves(forcedCapture.getSource(), forcedCapture.getMoves());
-            CheckersMan target = move.getMoves().get(ThreadLocalRandom.current().nextInt(move.getMoves().size()));
-            CheckersMan source = move.getSource();
-
+            // Create forced moves
+            CheckersMoves forcedMoves = new CheckersMoves(forcedCapture.getSource(), forcedCapture.getMoves());
+            // Set source and get random target
+            CheckersMan source = forcedMoves.getSource();
+            CheckersMan target = forcedMoves.getMoves().get(ThreadLocalRandom.current().nextInt(forcedMoves.getMoves().size()));
             return board.move(source.x, source.y, target.x, target.y, BLACK);
         }
 
-        CheckersResult result = minMaxStart(board, depth, side, true);
+        // If there is no captured moves start min-max algorithm and find best and optimal move
+        CheckersResult result = minMaxStart(board, depth, side);
         try {
             return board.move(result.getSource().x, result.getSource().y, result.getTarget().x, result.getTarget().y, side);
         } catch (GamesException e) {
@@ -52,36 +81,57 @@ public class MinMaxAlgorithm {
         throw new GamesException(GamesException.GamesErrors.AI_ERROR);
     }
 
-    private CheckersResult minMaxStart(CheckersBoard board, int depth, Side side, boolean maximizingPlayer) {
+    /**
+     * Starts min-max algorithm.
+     *
+     * @param board Reference to checkers board
+     * @param depth Value how far search in tree
+     * @param side  Side of computer
+     * @return Returns CheckersResult with information where to move draughtsman
+     */
+    private CheckersResult minMaxStart(CheckersBoard board, int depth, Side side) {
+        // Start with maximizing
+        boolean maximizingPlayer = true;
+
         double alpha = Double.NEGATIVE_INFINITY;
         double beta = Double.POSITIVE_INFINITY;
 
+        // Get all possible moves on board
         List<CheckersMoves> possibleMoves = board.getMoves(side, true);
 
+        // Check if possible moves list is empty
         if (possibleMoves.isEmpty())
+            // Set possible moves with different captures flag
             possibleMoves = board.getMoves(side, false);
 
 
+        // If list is still empty return null and throw error
         if (possibleMoves.isEmpty())
             return null;
 
+        // Initialize list of evaluated values
         List<Double> heuristics = new ArrayList<>();
 
+        // Reference for copied board
         CheckersBoard tempBoard = null;
         for (int i = 0; i < possibleMoves.size(); i++) {
             tempBoard = board.copy();
+            //Make move on temp board and do it recursively (depends on depth)
             move(side, possibleMoves, tempBoard, i);
-            heuristics.add(minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), !maximizingPlayer, alpha, beta));
+            maximizingPlayer = !maximizingPlayer;
+            heuristics.add(minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), maximizingPlayer, alpha, beta));
         }
 
         double maxHeuristics = Double.NEGATIVE_INFINITY;
 
+        // Find max evaluated value
         for (int i = heuristics.size() - 1; i >= 0; i--) {
             if (heuristics.get(i) >= maxHeuristics) {
                 maxHeuristics = heuristics.get(i);
             }
         }
 
+        // Remove all values below max evaluated value
         for (int i = 0; i < heuristics.size(); i++) {
             if (heuristics.get(i) < maxHeuristics) {
                 heuristics.remove(i);
@@ -90,21 +140,25 @@ public class MinMaxAlgorithm {
             }
         }
 
-
+        // Return random move from all best moves
         CheckersResult checkersResult = new CheckersResult();
         CheckersMoves move = possibleMoves.get(ThreadLocalRandom.current().nextInt(possibleMoves.size()));
-        CheckersMan target = move.getMoves().get(0);
-        CheckersMan source = move.getSource();
-        checkersResult.setSource(source);
-        checkersResult.setTarget(target);
+        checkersResult.setSource(move.getSource());
+        checkersResult.setTarget(move.getMoves().get(0));
         return checkersResult;
     }
 
+    /**
+     * Searching tree with all possible moves to find the best and optimal move. Using evaluation function
+     * algorithm searches for max path among the computer's moves and the minimum among the moves of the opponent.
+     */
     private double minMaxAlgorithm(CheckersBoard board, int depth, Side side, boolean maxOrMin, double alpha, double beta) {
+        // If depth is 0 return evaluated value
         if (depth <= 0) {
             return getHeuristic(board);
         }
 
+        //Get all possible moves from copied
         List<CheckersMoves> possibleMoves = board.getMoves(side, true);
         if (possibleMoves.isEmpty())
             possibleMoves = board.getMoves(side, false);
@@ -117,7 +171,8 @@ public class MinMaxAlgorithm {
                 tempBoard = board.copy();
                 move(side, possibleMoves, tempBoard, i);
 
-                double result = minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), !maxOrMin, alpha, beta);
+                maxOrMin = !maxOrMin;
+                double result = minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), maxOrMin, alpha, beta);
 
                 initial = Math.max(result, initial);
                 alpha = Math.max(alpha, initial);
@@ -127,18 +182,12 @@ public class MinMaxAlgorithm {
             }
         } else {
             initial = Double.POSITIVE_INFINITY;
-            for (CheckersMoves possibleMove : possibleMoves) {
+            for (int i = 0; i < possibleMoves.size(); i++) {
                 tempBoard = board.copy();
-                try {
-                    CheckersMoves move = possibleMove;
-                    CheckersMan target = move.getMoves().get(ThreadLocalRandom.current().nextInt(move.getMoves().size()));
-                    CheckersMan source = move.getSource();
-                    tempBoard.move(source.x, source.y, target.x, target.y, side);
-                } catch (GamesException e) {
-                    e.printStackTrace();
-                }
+                move(side, possibleMoves, tempBoard, i);
 
-                double result = minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), !maxOrMin, alpha, beta);
+                maxOrMin = !maxOrMin;
+                double result = minMaxAlgorithm(tempBoard, depth - 1, flipSide(side), maxOrMin, alpha, beta);
 
                 initial = Math.min(result, initial);
                 alpha = Math.min(alpha, initial);
@@ -152,6 +201,7 @@ public class MinMaxAlgorithm {
         return initial;
     }
 
+    // Makes move
     private void move(Side side, List<CheckersMoves> possibleMoves, CheckersBoard tempBoard, int i) {
         try {
             CheckersMoves move = possibleMoves.get(i);
@@ -163,6 +213,10 @@ public class MinMaxAlgorithm {
         }
     }
 
+    /**
+     * Evaluation function that determines the current state. Using number of draughtsmans and the weight of the draughtsman (queen or normal)
+     * (or with fuzzy logic that determines value of current stage of game) returns value.
+     */
     private double getHeuristic(CheckersBoard b) {
         double queenWeight = 3;
         double pieceWeight = 1;
@@ -190,6 +244,9 @@ public class MinMaxAlgorithm {
         return result;
     }
 
+    /**
+     * Changes side of player for algorithm purposes
+     */
     private Side flipSide(Side side) {
         if (side == Side.BLACK)
             return Side.WHITE;
