21d7fa99b35f3f09798b74ee5d8cb952dc3b9917
Kamil Wasilewski <bbeny123@gmail.com>
2018-12-19 01:52:51 +0100

    local environment settings

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Run inside project main dir:<br/>
 mvn install:install-file -Dfile=lib\jFuzzyLogic_v3.0.jar -DgroupId=net.sourceforge.jFuzzyLogic -DartifactId=jFuzzyLogic -Dversion=3.0.0 -Dpackaging=jar -DgeneratePom=true
 
 Completion of missing data inside <b><i>application.yaml</i></b> is required for the proper working.<br /><br />
-
-
--- src/main/java/pl/beny/nsai/util/MailUtil.java
+++ src/main/java/pl/beny/nsai/util/MailUtil.java
@@ -14,21 +14,25 @@ public class MailUtil {
     private static String url;
     private static JavaMailSender mailSender;
     private static MessageSource messageSource;
+    private static boolean mailEnabled;
 
     @Autowired
-    private MailUtil(JavaMailSender mailSender, MessageSource messageSource, @Value("${registration.url:http://localhost:8081/register/activate?token=}") String url) {
+    private MailUtil(JavaMailSender mailSender, MessageSource messageSource, @Value("${registration.mail.url:http://localhost:8081/register/activate?token=}") String url, @Value("${registration.mail.enable:false}") boolean mailEnabled) {
         MailUtil.mailSender = mailSender;
         MailUtil.messageSource = messageSource;
         MailUtil.url = url;
+        MailUtil.mailEnabled = mailEnabled;
     }
 
     public static void sendActivationEmail(String email, String token) {
-        String URL = url + token;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject(messageSource.getMessage("registration.email.subject", new Object[]{URL}, LocaleContextHolder.getLocale()));
-        message.setText(messageSource.getMessage("registration.email.activation", new Object[]{URL}, LocaleContextHolder.getLocale()));
-        mailSender.send(message);
+        if (mailEnabled) {
+            String URL = url + token;
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject(messageSource.getMessage("registration.email.subject", new Object[]{URL}, LocaleContextHolder.getLocale()));
+            message.setText(messageSource.getMessage("registration.email.activation", new Object[]{URL}, LocaleContextHolder.getLocale()));
+            mailSender.send(message);
+        }
     }
 
 }
